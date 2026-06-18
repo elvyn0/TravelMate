a17fbba7c58aa4ae3f98c3e28ab56e055eabf97c
Elvyn <122730895+elvyn0@users.noreply.github.com>
2026-06-18 22:47:01 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const corsOptions = {
-  origin: "https://travel-mate-khaki.vercel.app/" || "http://localhost:5173",
+  origin: ["https://travel-mate-khaki.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
